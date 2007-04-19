a53b1aacbf04f48c3691312475b66f86d3ae4db6
rogerpack2005 <rogerpack2005@23766b47-6328-0410-be0f-1199edc0a802>
2007-04-19 17:25:32 +0000

    should handle exceptions better...

--- trunk/runBoth.sh
+++ trunk/runBoth.sh
@@ -1,6 +1,5 @@
-nohup ./run_forever.sh "python establishAlien.py " >> nothing.establishAlien.py.out.runBoth.nothing &
-nohup ./run_forever.sh "python proxyServer.py 3220 10005 8000" >> nothing.proxyServer.py.out.runBoth.nothing &
-#python proxyServer.py 3222 10006 8001
-nohup ./run_forever.sh "python proxyServer.py 3221 10006 8001"  >> nothing.proxyServer.py.out.runBoth.nothing &
-nohup ./run_forever.sh "python proxyServer.py 3222 10007 8002"  >> nothing.proxyServer.py.out.runBoth.nothing &
+nohup ./run_forever.sh "python establishAlien.py " >> /home/byu_p2pweb/logs/nothing.establishAlien.py.out.runBoth.nothing &
+nohup ./run_forever.sh "python proxyServer.py 3220 10005 8000" >> /home/byu_p2pweb/logs/nothing.proxyServer.py.3220.out.runBoth.nothing &
+nohup ./run_forever.sh "python proxyServer.py 3221 10006 8001"  >> /home/byu_p2pweb/logs/nothing.proxyServer.py.3221.out.runBoth.nothing &
+nohup ./run_forever.sh "python proxyServer.py 3222 10007 8002"  >> /home/byu_p2pweb/logs/nothing.proxyServer.py.3222.out.runBoth.nothing &
 
